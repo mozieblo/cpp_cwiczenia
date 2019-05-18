08b36eb80f7823095734cee087781dc9440fa56e
mozieblo <m.ozieblo@wp.pl>
2019-05-18 17:43:16 +0200

    counting cals for cm

--- cw1.3_XIV.cpp
+++ cw1.3_XIV.cpp
@@ -1,7 +1,21 @@
+// program przeliczający cale na centymetry
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    int cale;       // przechowuje dane wejściowe
+    double cm;      // do wpisania wyniku
+    double przelicznik = 2.54;      // przelicznik cale na cm 1 cal = 2.54 cm
+
+    cout << "Podaj cale: ";
+    cin >> cale;                    // przyjęcie danej z klawiatury
+
+    cm = cale * przelicznik;        // właściwe przeliczenie
+
+    cout << "\n";                   // to samo co cout << endl;
+
+    cout << cale << " cali - to jest: " << cm << " centymetrow\n";
+
     
 }
